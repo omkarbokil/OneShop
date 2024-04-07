60dddd980dfa2266097310e6d6c8d16db611ecf8
Omkar <omkarbokil22@gmail.com>
2024-04-07 22:05:28 +0530

    font additio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'arimo'         : ['Arimo'],
+        'rubik'         : ['Rubik'],
+        'robotomono'    : ['Roboto Mono'],
+        'roboto'        : ['Roboto'],
+        'figtree'       : ['Figtree'],
+        'spacegrotesk'  : ['Space Grotesk'],
+        'worksans'      : ['Work Sans'],
+        'leaguespartan' : ['League Spartan'],
+        'raleway'       : ['Raleway'],
+        'expletussans'  : ['Expletus Sans']
+      },
+
+      animation: {
+        nav: 'nav 0.5s ease-in forwards',
+      },
+
+      keyframes: {
+        nav: {
+          '0%': {
+              left:'0',
+              width:'0'
+            },
+            '50%': {
+              left:'0',
+              width:'100%'
+            },
+            '100%': {
+              left:'0',
+              width:'100%'
+            }
+        }
+      }
+    },
   },
   plugins: [],
 }
