2a931c86e22c118f061c6d7403f37c5b356425ea
Omkar <omkarbokil22@gmail.com>
2024-04-09 16:10:27 +0530

    commit

--- JS/oneshop.js
+++ JS/oneshop.js
@@ -52,6 +52,8 @@ offer.forEach((val, ind) => {
 let collapsibleMenuOpen = document.querySelector("#collapsible-menu-open");
 let collapsibleMenuClose = document.querySelector("#collapsible-menu-close");
 let collapsibleMenu = document.querySelector("#collapsible-menu");
+let navbar = document.querySelector("#navbar");
+let footer = document.querySelector("#footer");
 
 collapsibleMenuOpen.addEventListener("click", () => {
      // collapsibleMenu.classList.toggle("hidden");
--- JS/profile.js
+++ JS/profile.js
@@ -8,3 +8,22 @@ upIcon.addEventListener("mouseenter", () =>{
 upIcon.addEventListener("mouseleave", () =>{
      uploadPicture.classList.add("hidden");
 })
+
+// Collapsible Menu
+let collapsibleMenuOpen = document.querySelector("#collapsible-menu-open");
+let collapsibleMenuClose = document.querySelector("#collapsible-menu-close");
+let collapsibleMenu = document.querySelector("#collapsible-menu");
+let navbar = document.querySelector("#navbar");
+let footer = document.querySelector("#footer");
+
+collapsibleMenuOpen.addEventListener("click", () => {
+     // collapsibleMenu.classList.toggle("hidden");
+     collapsibleMenu.style.top = 0;
+     // collapsibleMenuClose.classList.toggle("hidden");
+})
+
+collapsibleMenuClose.addEventListener("click", () => {
+     // collapsibleMenu.classList.toggle("hidden");
+     collapsibleMenu.style.top = "-50rem";
+     // collapsibleMenuClose.classList.toggle("hidden");
+})
