fcfe296e47c9811381b79ab192ec46ed06dd4e4f
Omkar <omkarbokil22@gmail.com>
2024-04-10 18:57:21 +0530

    cancel order button effect added

---
+++ JS/returns.js
@@ -0,0 +1,16 @@
+let returnProducts  =    document.querySelectorAll(".return-products");
+let cancelOrder     =    document.querySelectorAll(".cancel-order");
+let body            =    document.getElementsByTagName("body");
+let loader          =    document.querySelector(".loader");
+
+// Hide the element After clicking on cancel order button
+cancelOrder.forEach((val, ind) => {
+     val.addEventListener("click", () => {
+          cancelOrder[ind].disabled = true;
+          loader.classList.toggle("hidden");
+          setTimeout(() => {
+               returnProducts[ind].classList.toggle("hidden");
+               loader.classList.toggle("hidden");
+          }, 3000);
+     })
+})
