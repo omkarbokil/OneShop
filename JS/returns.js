e31b85fc00dd126c755dff93763019d9618f51c0
Omkar <omkarbokil22@gmail.com>
2024-04-10 19:23:02 +0530

    changes on return screen

--- JS/returns.js
+++ JS/returns.js
@@ -7,9 +7,11 @@ let loader          =    document.querySelector(".loader");
 cancelOrder.forEach((val, ind) => {
      val.addEventListener("click", () => {
           cancelOrder[ind].disabled = true;
+          loader.classList.add("bg-white/70");
           loader.classList.toggle("hidden");
           setTimeout(() => {
                returnProducts[ind].classList.toggle("hidden");
+               loader.classList.remove("bg-white/70");
                loader.classList.toggle("hidden");
           }, 3000);
      })
